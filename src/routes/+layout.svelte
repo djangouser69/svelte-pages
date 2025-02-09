0fafd3b24e083948053907e9e9dc9881d8797d3d
djangouser69 <ksidoctor@outlook.com>
2025-02-08 20:54:41 -0600

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  const { children } = $props();
+</script>
+
+<nav>
+  <a href="/">index</a>
+  <a href="/about">about</a>
+  <a href="/test">test</a>
+  <a href=""></a>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let count = $state(0);
+  let doubled = $derived(count * 2);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<button onclick={() => count++}>{count}</button>
+<button onclick={() => (count *= 2)}>{doubled}</button>
